f83e18875a235f4556b84001acc09d85210c5079
Bimme Audrey Z <bimmeaudrey@gmail.com>
2023-09-08 21:33:13 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-const desiredTime = new Date();
-desiredTime.setUTCHours(24);
-desiredTime.setUTCMinutes(1440);
-desiredTime.setUTCSeconds(86400);
-desiredTime.setUTCMilliseconds(0);
-
-
-const timestamp = desiredTime.getTime();
-
-const timeElement = document.getElementById("time");
-timeElement.innerHTML = timestamp.toString();
-
-console.log(timestamp);
+document.getElementById('time').innerHTML = Date.now();
